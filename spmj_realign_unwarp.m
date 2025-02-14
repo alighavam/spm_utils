8850d533bd53720253bedd929e21fd03780d0c38
Ali Ghavampour <53136447+alighavam@users.noreply.github.com>
2025-02-14 18:05:06 -0500

    rewrote

--- spmj_realign_unwarp.m
+++ spmj_realign_unwarp.m
@@ -1,4 +1,4 @@
-function spmj_realign_unwarp(subj_name, run_names, varargin)
+function spmj_realign_unwarp(epi_list, fmap_list, varargin)
 % spmj_realign_unwarp(subj_name, run_names)
 % INPUT: 
 %   subj_name:  Name for subdirectory and file-prefix (i.e. 's05')
@@ -19,14 +19,10 @@ function spmj_realign_unwarp(subj_name, run_names, varargin)
 % B.Multi session experiment:
 % sess_names = {'ses-01','ses-02}
 % run_names = {{'run-01' 'run-02','run-03','run-04' },{'run-01' 'run-02'}}
-raw_name = 'run'; 
-rawdata_dir = ''; 
-fmap_dir = ''; 
-base_dir = ''; 
-sess_names = {}; 
-rtm = 0;    % register to mean or first volume. default register to first volume of the first run.
 
-vararginoptions(varargin,{'prefix', 'fmap_dir','rawdata_dir','base_dir','target_dir','rtm','raw_name','sess_names'}); 
+rtm = 0;    % register to mean or first volume. default register to first volume of the first run.
+prefix = 'u';
+vararginoptions(varargin,{'prefix','rtm'}); 
 
 %_______DEFAULTS_________________________________
 J.eoptions.quality = 0.9;
@@ -50,54 +46,23 @@ J.uwroptions.uwwhich = [2 1]; %[2 1]: with mean image. [2 0]: without
 J.uwroptions.rinterp = 4;                                                                            
 J.uwroptions.wrap = [0 1 0];  %  wrap-around in the [x y z] direction during the reslicing (wrap)                                                                                
 J.uwroptions.mask = 1;                                                                                
-J.uwroptions.prefix = 'u'; 
-
-if isempty(rawdata_dir)
-    rawdata_dir = fullfile(base_dir,'imaging_data_raw');
-end
-if isempty(fmap_dir)
-    fmap_dir = fullfile(base_dir,'fieldmaps');
-end 
+J.uwroptions.prefix = prefix; 
 
 %_______images and fieldmap definition_________________________________
-% Flat hierarchy one imaging session 
-if (isempty(sess_names))
-    for j=1:numel(run_names)
-        file_name = fullfile(rawdata_dir,subj_name,sprintf('%s_%s.nii',subj_name,run_names{j}));
-        V = nifti(char(file_name));
-        if (V.dat.dim==3)
-            scans{1}= file_name;
-        else 
-            imageNumber=1:V.dat.dim(4); 
-            for i= 1:numel(imageNumber)
-                scans{i}= sprintf('%s,%d',file_name,i);
-            end
-        end
-        J.data(j).scans = scans';
-        clear scans; 
-%         J.data(j).pmscan = {fullfile(fmap_dir ,subj_name,['vdm5_sc',subj_name,'_phase_',run_names{j},'.nii,1'])};
-        J.data(j).pmscan = {fullfile(fmap_dir ,subj_name,sprintf('vdm5_sc%s_phase_run_%d.nii,1', subj_name, j))};
-    end
-else  % Nested hierarchy with different subfolders for different sessions 
-    indx = 1; 
-    for s=1:numel(sess_names)
-        for j=1:numel(run_names{s})
-            file_name = fullfile(rawdata_dir,subj_name,sess_names{s},sprintf('%s_%s.nii',subj_name,run_names{s}{j}));
-            V = nifti(file_name);
-            if (length(V.dat.dim)==3) % 3dimage (likely sbref)
-                scans{1}= file_name;
-            else 
-                imageNumber=1:V.dat.dim(4); 
-                for i= 1:numel(imageNumber)
-                    scans{i}= sprintf('%s,%d',file_name,i);
-                end
-            end
-            J.data(indx).scans = scans';
-            J.data(indx).pmscan = {fullfile(fmap_dir,subj_name,sess_names{s},sprintf('vdm5_sc%s_%s_phasediff_run_%d.nii',subj_name,sess_names{s},j))};
-            indx = indx+1; 
-            clear scans; 
+for j = 1:numel(epi_list)
+    epi_name = epi_list{j};
+    V = nifti(char(epi_name));
+    if (V.dat.dim==3)
+        scans{1}= epi_name;
+    else 
+        imageNumber=1:V.dat.dim(4); 
+        for i = 1:numel(imageNumber)
+            scans{i} = sprintf('%s,%d',epi_name,i);
         end
     end
+    J.data(j).scans = scans';
+    clear scans; 
+    J.data(j).pmscan = fmap_list(j);
 end
 
 matlabbatch{1}.spm.spatial.realignunwarp= J;
